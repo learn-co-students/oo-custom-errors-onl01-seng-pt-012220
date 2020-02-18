8ce072466132b2db80aed1099ccad2ab882d8634
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-02-18 23:50:05 +0000

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,16 +1,4 @@
-#class Person
-  #attr_accessor :partner, :name
 
-  #def initialize(name)
-    #@name = name
-  #end
-
-  #def get_married(person)
-  #  self.partner = person
-   # person.partner = self
-  #end
-
-#end
 class Person
   attr_accessor :partner, :name
  
