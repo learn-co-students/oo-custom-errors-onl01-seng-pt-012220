ea154b27f0ebf6401c4737d14bf0baf7d10d5ec0
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-02-18 23:49:39 +0000

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,16 +1,39 @@
+#class Person
+  #attr_accessor :partner, :name
+
+  #def initialize(name)
+    #@name = name
+  #end
+
+  #def get_married(person)
+  #  self.partner = person
+   # person.partner = self
+  #end
+
+#end
 class Person
   attr_accessor :partner, :name
-
+ 
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      raise PartnerError
+    else
+      person.partner = self
+    end
   end
-
+ 
+  class PartnerError < StandardError
+  end
 end
+ 
+beyonce = Person.new("Beyonce")
+beyonce.get_married("Jay-Z")
+puts beyonce.name
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
